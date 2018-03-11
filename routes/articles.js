b9909339ed41e54fa386b12f1c6b40273d383aed
Calvin Yah <calvin.yah@gmail.com>
2018-03-11 02:54:51 +0000

    Added a new feature to extract image from a given URL

--- routes/articles.js
+++ routes/articles.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router  = express.Router();
-const bcrypt = require('bcrypt');
+
 const cookieSession = require('cookie-session');
 const bodyParser = require("body-parser");
 
