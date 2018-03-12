48be3336c996c2f9173271380b9628bf6a6ffc74
Calvin Yah <calvin.yah@gmail.com>
2018-03-12 01:15:20 +0000

    Styled article page

--- routes/articles.js
+++ routes/articles.js
@@ -40,7 +40,7 @@ router.get("/:article_id",(req,res) =>{
               templateVars.comments.push(comment);
 
             }
-            // console.log(templateVars);
+             console.log(templateVars.comment);
             res.render("article", templateVars);
           });
 
